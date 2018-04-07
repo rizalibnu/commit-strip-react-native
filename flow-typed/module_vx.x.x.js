c65e01605d368c33821bc9772ed78fe005421226
Rizal Ibnu <muhamadrizalibnuabdulah@gmail.com>
2018-04-07 14:11:07 +0700

    Update flow typed

--- flow-typed/module_vx.x.x.js
+++ flow-typed/module_vx.x.x.js
@@ -4,24 +4,6 @@ declare module 'react-navigation' {
 declare module 'react-native' {
   declare module.exports: any;
 }
-declare module 'react-redux' {
-  declare module.exports: any;
-}
-declare module 'react-redux' {
-  declare module.exports: any;
-}
-declare module 'redux-form' {
-  declare module.exports: any;
-}
-declare module 'redux-devtools-extension' {
-  declare module.exports: any;
-}
-declare module 'redux' {
-  declare module.exports: any;
-}
-declare module 'redux-saga' {
-  declare module.exports: any;
-}
 declare module 'react-native-paper' {
   declare module.exports: any;
 }
